fed920e947a28387b0094e0f7ccd54488af68917
Bolivian33000 <139071316+Bolivian33000@users.noreply.github.com>
2024-12-13 10:02:51 -0600

    Modify TodoCard.jsx

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -8,21 +8,15 @@ Each `TodoCard`:
 - Calls the provided `handleDeleteTodo` function when the delete button is clicked.
 */}
 
-// Create the handleUrgentTodo in App.jsx
-// pass the handleUrgentTodo as props to the TodoCard function
-// on click, trigger the handleUrgentTodo function in the TodoCard component
-
-
 export default function TodoCard(props) {
-  const { children, handleDeleteTodo, index, handleEditTodo, handleUrgentTodo = [] } = props;  // destructure out handleUrgentTodo in the props a bit
-
+  const { children, handleDeleteTodo, index, handleEditTodo, handleUrgentTodo, isUrgent } = props; 
   // Destructure props:
   // - `children`: The content (text) of the to-do item, passed from TodoList.
   // - `handleDeleteTodo`: A function to delete a to-do item, passed from the parent component (App).
   // - `index`: The position of this to-do item in the list, needed for deletion.
 
   return (
-    <li className={`todoItem ${isUrgent ? "urgent": ""}`}>
+    <li className={`todoItem ${isUrgent ? "urgent" : ""}`} > {/* className in JavaScript world -- adding conditional className "urgent" depending on the boolean value of isUrgent}
       {/* Render the to-do text */}
       {children}
 
@@ -41,20 +35,14 @@ export default function TodoCard(props) {
         >
           <i className="fa-solid fa-trash"></i> {/* FontAwesome delete icon */}
         </button>
-        
-        {/* Label as Urgent Button */}
+
         <button 
-           onClick={() => handleUrgentTodo(index)}
-           // onClick={() => handleUrgentTodo(index)}  // Need the handleUrgentTodo function to be created in our App.jsx component
-          // What I would like to do is, on the click, increase the size of the button, along with moving this todo item to the top
+          onClick={() => handleUrgentTodo(index)}
+              // When clicked, this calls the handleUrgentTodofunction
         >
-          
-          {isUrgent ? "Unmark Urgent" : "Mark as Urgent"} {/* if isUrgent is true: Display "Unmark Urgent"; if false, display "Mark as Urgent" */}
+          <i className="fa-solid fa-circle-exclamation"></i>
 
-          <i className="fa-solid fa-circle-exclamation"></i> {/* FontAwesome exclamation icon */}
         </button>
-
-      
 
 
       </div>
@@ -62,11 +50,14 @@ export default function TodoCard(props) {
   );
 }
 
+
+
 {/* Notes:
 - `children` allows `TodoCard` to be reusable for any content passed between its opening and closing tags in JSX.
 - Props are used to pass data (like `index`) and functions (like `handleDeleteTodo`) from the parent component.
 - Actions (Edit/Delete) are currently set up, but only Delete has functionality in this example.
 */}
+
 
 
 
